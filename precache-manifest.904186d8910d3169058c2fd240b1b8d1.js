0946ce69d959aa139d68591e2724e0a58649993c
Efe Erginbas <erginbas@berkeley.edu>
2025-02-20 21:23:46 +0000

    Updates

---
+++ precache-manifest.904186d8910d3169058c2fd240b1b8d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6654235e08d56509219cd3a2be5e0b61",
+    "url": "/spex-webapp/index.html"
+  },
+  {
+    "revision": "aa97f81b79a06b34b54c",
+    "url": "/spex-webapp/static/css/main.9cee2060.chunk.css"
+  },
+  {
+    "revision": "274d584adbaabb705718",
+    "url": "/spex-webapp/static/js/2.ce8fcd6b.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/spex-webapp/static/js/2.ce8fcd6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3984322b83277e3643ed",
+    "url": "/spex-webapp/static/js/3.37578189.chunk.js"
+  },
+  {
+    "revision": "aa97f81b79a06b34b54c",
+    "url": "/spex-webapp/static/js/main.2ffbd7bd.chunk.js"
+  },
+  {
+    "revision": "00465d11237f78855120",
+    "url": "/spex-webapp/static/js/runtime-main.ef3f7e40.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spex-webapp/precache-manifest.dedf32c34fd5d9258d853a3f0ab4599c.js"
+  "/spex-webapp/precache-manifest.904186d8910d3169058c2fd240b1b8d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spex-webapp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
